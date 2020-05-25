9180e114b5e0e116d5bc0033ea1c46cef826b86c
MKI_Rahul <rahul.privat@web.de>
2020-05-25 13:28:13 +0200

    Created Arena and Pokemon class.

---
+++ Pokemonarena/Arena.py
@@ -0,0 +1,16 @@
+# Arena
+class Arena:
+
+    # Konstruktor
+    def __init__(self):
+        
+        
+
+    # Lade die Pokemon aus der csv
+    def loadPokemons():
+
+
+
+    # Lässt die Pokemon gegeneinander Kämpfen
+    def fight():
+        
---
+++ Pokemonarena/Pokemon.py
@@ -0,0 +1,13 @@
+# Pokemon
+class Pokemon:
+
+    # Konstruktor
+    def __init__(self, name, hp, attack, defense, specialAttack, specialDefense, speed):
+        self.name = name
+        self.hp = hp
+        self.attack = attack
+        self.defense = defense
+        self.specialAttack = specialAttack
+        self.specialDefense = specialDefense
+        self.speed = speed
+        self.score
